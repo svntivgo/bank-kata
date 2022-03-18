fb4d0d3d2255ad80e247650ef6f541ac8d0641be
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-18 02:43:41 -0500

    test deposito

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,12 +1,17 @@
+import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.Date;
 import java.util.List;
 
 public class Account {
+    BigDecimal amount;
+    LocalDate date;
 
-    void deposit(int amount, Instant date) {
-
+    void deposit(BigDecimal amount, LocalDate date) {
+        System.out.println("Monto: "+amount+", fecha: "+date);
     }
 
     void withdraw(int amount, Instant date){
--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -1,9 +1,30 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Currency;
+import java.util.Date;
+
 public class AcceptanceTest {
+    private BigDecimal amountOf(int number) {
+        return BigDecimal.valueOf(number);
+    }
+
+    private LocalDate date(String dateOnString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate parsedDate = LocalDate.parse(dateOnString, formatter);
+        return parsedDate;
+    }
 
     @Test
-    public void shouldWhatever() {
+    public void deposito() {
+        Account cuenta = new Account();
+        cuenta.deposit(amountOf(500), date("03/01/2020"));
     }
+
+
 
 }
